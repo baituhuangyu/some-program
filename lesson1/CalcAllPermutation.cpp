02d37c4ed63f48a9dc9b2a84263a26ffc0dfa4b1
yu.huang <yu.huang@socialcredits.cn>
2016-07-23 00:34:26 +0800

    add

---
+++ lesson1/CalcAllPermutation.cpp
@@ -0,0 +1,34 @@
+//
+// Created by hy on 16-7-22.
+//
+
+#include <iostream>
+using namespace std;
+
+
+void CalcAllPermutation(string perm, int from, int to) {
+//void CalcAllPermutation(char* perm, int from, int to) {
+    if (to <= 1) {
+        return;
+    }
+    if (from == to) {
+        for (int i = 0; i <= to; i++) {
+            cout << perm[i];
+        }
+        cout << endl;
+    }
+    else {
+        for (int j = from; j <= to; j++) {
+            swap(perm[j], perm[from]);
+            CalcAllPermutation(perm, from + 1, to);
+            swap(perm[j], perm[from]);
+        }
+    }
+}
+
+
+int main(void){
+//    char* abc = "abc";
+    string abc = "abc";
+    CalcAllPermutation(abc, 0, 2);
+}
